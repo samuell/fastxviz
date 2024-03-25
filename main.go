f4f5d456a71181e4e01f27dbbd0864f58472a1cd
Samuel Lampa <samuel.lampa@scilifelab.se>
2024-03-25 11:14:23 +0100

    Add code to plot read lengths on the terminal

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"compress/gzip"
+	"flag"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+)
+
+func main() {
+	infile := flag.String("input", "input.fq", "Input file in fastq format")
+
+	flag.Parse()
+
+	file, err := os.Open(*infile)
+	checkMsg(err, "Could not open file")
+
+	var scanner *bufio.Scanner
+	if strings.HasSuffix(*infile, ".gz") {
+		ungzipper, err := gzip.NewReader(file)
+		checkMsg(err, "Could not create GZip reader")
+		scanner = bufio.NewScanner(ungzipper)
+	} else {
+		scanner = bufio.NewScanner(file)
+	}
+
+	// Compute length of lines
+	lengths := []int{}
+	lineNo := 1
+	for scanner.Scan() {
+		line := scanner.Text()
+		if lineNo%4 == 2 {
+			//fmt.Printf("Line %7d: %s\n", lineNo, line)
+			lengths = append(lengths, len(line))
+		}
+		lineNo++
+	}
+	checkMsg(scanner.Err(), "Error scanning text")
+	sort.Ints(lengths)
+
+	for i, length := range lengths {
+		lenStr := ""
+		for l := 0; l < length; l++ {
+			lenStr = lenStr + "*"
+		}
+		fmt.Printf("Length %7d: %s\n", i, lenStr)
+	}
+}
+
+func checkMsg(err error, msg string) {
+	if err != nil {
+		fmt.Println(msg, err)
+	}
+}
